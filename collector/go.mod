19aebc47d9ead7c5ac8c55b9a914dbfd612f9fe6
Neil Desai <ndesai1996@gmail.com>
2025-09-03 09:55:37 -0500

    Add gRPC server for collecting moisture data

--- collector/go.mod
+++ collector/go.mod
@@ -1,3 +1,16 @@
 module github.com/ndesai96/houseplants/collector
 
 go 1.25.0
+
+require (
+	github.com/ndesai96/houseplants/protobuf v0.0.0-20250902192122-bb1b241492f2
+	google.golang.org/grpc v1.75.0
+)
+
+require (
+	golang.org/x/net v0.43.0 // indirect
+	golang.org/x/sys v0.35.0 // indirect
+	golang.org/x/text v0.28.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250826171959-ef028d996bc1 // indirect
+	google.golang.org/protobuf v1.36.8 // indirect
+)
--- collector/main.go
+++ collector/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/ndesai96/houseplants/protobuf"
+	"google.golang.org/grpc"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		panic(err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterCollectorServer(s, &collector{})
+
+	if err = s.Serve(lis); err != nil {
+		panic(err)
+	}
+
+	log.Println("Listening on port 50051...")
+}
+
+type collector struct {
+	pb.UnimplementedCollectorServer
+}
+
+func (c *collector) SendMoistureData(_ context.Context, req *pb.SendMoistureDataRequest) (*pb.SendMoistureDataResponse, error) {
+	log.Println("Received: ", req)
+	return &pb.SendMoistureDataResponse{}, nil
 }
