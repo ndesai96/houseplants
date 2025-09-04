dd2b41ab1bb573f9029fadcc63b93d53086796b0
Neil Desai <ndesai1996@gmail.com>
2025-09-04 12:47:19 -0500

    Add mutual TLS and deviceID handling in collector and gateway

--- collector/main.go
+++ collector/main.go
@@ -2,26 +2,59 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"log"
 	"net"
+	"os"
 
 	pb "github.com/ndesai96/houseplants/protobuf"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+var (
+	serverCert = os.Getenv("SERVER_CERT")
+	serverKey  = os.Getenv("SERVER_KEY")
+	caCertFile = os.Getenv("CA_CERT")
 )
 
 func main() {
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
-		panic(err)
+		log.Fatalf("failed to listen: %v", err)
 	}
 
-	s := grpc.NewServer()
+	s := grpc.NewServer(grpc.Creds(buildTransportCredentials()))
 	pb.RegisterCollectorServer(s, &collector{})
 
 	log.Println("Listening on port 50051...")
 	if err = s.Serve(lis); err != nil {
-		panic(err)
+		log.Fatalf("failed to serve: %v", err)
 	}
+}
+
+func buildTransportCredentials() credentials.TransportCredentials {
+	certificate, err := tls.LoadX509KeyPair(serverCert, serverKey)
+	if err != nil {
+		log.Fatalf("Failed to load server certificate: %s", err)
+	}
+
+	caCert, err := os.ReadFile(caCertFile)
+	if err != nil {
+		log.Fatalf("Failed to read CA certificate: %s", err)
+	}
+
+	caCertPool := x509.NewCertPool()
+	if ok := caCertPool.AppendCertsFromPEM(caCert); !ok {
+		log.Fatalf("Failed to append CA certificate to pool")
+	}
+
+	return credentials.NewTLS(&tls.Config{
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		Certificates: []tls.Certificate{certificate},
+		ClientCAs:    caCertPool,
+	})
 }
 
 type collector struct {
--- gateway/gateway.go
+++ gateway/gateway.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"crypto/tls"
+	"crypto/x509"
 	"log"
 	"net/url"
 	"os"
@@ -19,6 +20,7 @@ var (
 	collectorAddr  = os.Getenv("COLLECTOR_ADDR")
 	clientCert     = os.Getenv("CLIENT_CERT")
 	clientKey      = os.Getenv("CLIENT_KEY")
+	caCertFile     = os.Getenv("CA_CERT")
 	mqttBrokerAddr = os.Getenv("MQTT_BROKER_ADDR")
 	mqttClientID   = os.Getenv("MQTT_CLIENT_ID")
 	mqttTopic      = os.Getenv("MQTT_TOPIC")
@@ -47,15 +49,21 @@ func (g *gateway) start(ctx context.Context) {
 		log.Fatalf("failed to connect to MQTT broker: %s", token.Error())
 	}
 
-	if token := g.mqttClient.Subscribe(mqttTopic, 0, g.mqttMessageHandler); token.Wait() && token.Error() != nil {
+	if token := g.mqttClient.Subscribe(mqttTopic, 0, g.mqttMessageHandler(ctx)); token.Wait() && token.Error() != nil {
 		log.Fatalf("Failed to subscribe to MQTT topic: %s - %s\n", mqttTopic, token.Error())
 	}
 }
 
-func (g *gateway) mqttMessageHandler(_ mqtt.Client, msg mqtt.Message) {
-	topicPrefix := strings.TrimSuffix(mqttTopic, "#")
-	deviceID := strings.TrimPrefix(msg.Topic(), topicPrefix)
-	log.Printf("Received MQTT message for %s: %s\n", deviceID, msg.Payload())
+func (g *gateway) mqttMessageHandler(ctx context.Context) mqtt.MessageHandler {
+	return func(client mqtt.Client, msg mqtt.Message) {
+		topicPrefix := strings.TrimSuffix(mqttTopic, "#")
+		deviceID := strings.TrimPrefix(msg.Topic(), topicPrefix)
+		if _, err := g.collectorClient.SendMoistureData(ctx, &pb.SendMoistureDataRequest{
+			DeviceID: deviceID,
+		}); err != nil {
+			log.Printf("Failed to send moisture data to collector: %s\n", err)
+		}
+	}
 }
 
 func (g *gateway) stop() {
@@ -64,21 +72,34 @@ func (g *gateway) stop() {
 }
 
 func buildCollectorClient() (pb.CollectorClient, closeFunc) {
-	certificate, err := tls.LoadX509KeyPair(clientCert, clientKey)
-	if err != nil {
-		log.Fatalf("Failed to load client certificate: %s", err)
-	}
-
-	creds := credentials.NewTLS(&tls.Config{
-		Certificates: []tls.Certificate{certificate},
-	})
-
-	conn, err := grpc.NewClient(collectorAddr, grpc.WithTransportCredentials(creds))
+	conn, err := grpc.NewClient(collectorAddr, grpc.WithTransportCredentials(buildTransportCredentials()))
 	if err != nil {
 		log.Fatalf("Failed to create collector client: %s", err)
 	}
 
 	return pb.NewCollectorClient(conn), conn.Close
+}
+
+func buildTransportCredentials() credentials.TransportCredentials {
+	certificate, err := tls.LoadX509KeyPair(clientCert, clientKey)
+	if err != nil {
+		log.Fatalf("Failed to load client certificate: %s", err)
+	}
+
+	caCert, err := os.ReadFile(caCertFile)
+	if err != nil {
+		log.Fatalf("Failed to read CA certificate: %s", err)
+	}
+
+	caCertPool := x509.NewCertPool()
+	if ok := caCertPool.AppendCertsFromPEM(caCert); !ok {
+		log.Fatalf("Failed to append CA certificate to pool")
+	}
+
+	return credentials.NewTLS(&tls.Config{
+		Certificates: []tls.Certificate{certificate},
+		RootCAs:      caCertPool,
+	})
 }
 
 func buildMQTTClient() mqtt.Client {
