60dc7420ead200cec256eb49bee72c6cdc06d22f
Neil Desai <ndesai1996@gmail.com>
2025-09-05 16:29:14 -0500

    Refactor gateway to allow re-subscription after broker interruption

--- gateway/gateway.go
+++ gateway/gateway.go
@@ -32,20 +32,70 @@ type gateway struct {
 }
 
 func newGateway() *gateway {
-	collectorClient, closeCollectorClient := buildCollectorClient()
+	// Build gRPC Collector client
+	conn, err := grpc.NewClient(
+		collectorAddr,
+		grpc.WithTransportCredentials(tlsutil.BuildTransportCredentials()),
+	)
+	if err != nil {
+		log.Fatalf("Failed to create collector client: %s", err)
+	}
 
 	return &gateway{
-		collectorClient:      collectorClient,
-		closeCollectorClient: closeCollectorClient,
-		mqttClient:           buildMQTTClient(),
+		collectorClient:      pb.NewCollectorClient(conn),
+		closeCollectorClient: conn.Close,
 	}
 }
 
 func (g *gateway) start(ctx context.Context) {
+	g.mqttClient = g.buildMQTTClient(ctx)
+
 	if token := g.mqttClient.Connect(); token.Wait() && token.Error() != nil {
 		log.Fatalf("failed to connect to MQTT broker: %s", token.Error())
 	}
+}
 
+func (g *gateway) stop() {
+	_ = g.closeCollectorClient()
+	g.mqttClient.Disconnect(250)
+}
+
+func (g *gateway) buildMQTTClient(ctx context.Context) mqtt.Client {
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(mqttBrokerAddr)
+	opts.SetClientID(mqttClientID)
+
+	tlsConfig, err := tlsutil.BuildTLSConfig()
+	if err != nil {
+		log.Fatalf("Failed to build TLS config: %s", err)
+	}
+	opts.SetTLSConfig(tlsConfig)
+
+	opts.SetAutoReconnect(true)
+	opts.SetMaxReconnectInterval(5 * time.Second)
+
+	opts.SetCleanSession(true)
+	opts.SetKeepAlive(2 * time.Second)
+
+	opts.SetConnectRetry(true)
+	opts.SetConnectRetryInterval(5 * time.Second)
+
+	opts.SetConnectionAttemptHandler(func(broker *url.URL, tlsCfg *tls.Config) *tls.Config {
+		log.Printf("%s attempting to connect to MQTT broker: %s\n", mqttClientID, broker)
+		return tlsCfg
+	})
+	opts.SetOnConnectHandler(func(client mqtt.Client) {
+		log.Printf("%s connected to MQTT broker: %s\n", mqttClientID, mqttBrokerAddr)
+		g.subscribe(ctx)
+	})
+	opts.SetConnectionLostHandler(func(client mqtt.Client, err error) {
+		log.Printf("%s lost connection to MQTT broker: %s - %s\n", mqttClientID, mqttBrokerAddr, err)
+	})
+
+	return mqtt.NewClient(opts)
+}
+
+func (g *gateway) subscribe(ctx context.Context) {
 	if token := g.mqttClient.Subscribe(mqttTopic, 0, g.mqttMessageHandler(ctx)); token.Wait() && token.Error() != nil {
 		log.Fatalf("Failed to subscribe to MQTT topic: %s - %s\n", mqttTopic, token.Error())
 	}
@@ -76,55 +126,4 @@ func parseMoistureData(msg []byte) (*pb.MoistureData, error) {
 		return nil, err
 	}
 	return &data, nil
-}
-
-func (g *gateway) stop() {
-	_ = g.closeCollectorClient()
-	g.mqttClient.Disconnect(250)
-}
-
-func buildCollectorClient() (pb.CollectorClient, closeFunc) {
-	conn, err := grpc.NewClient(
-		collectorAddr,
-		grpc.WithTransportCredentials(tlsutil.BuildTransportCredentials()),
-	)
-	if err != nil {
-		log.Fatalf("Failed to create collector client: %s", err)
-	}
-
-	return pb.NewCollectorClient(conn), conn.Close
-}
-
-func buildMQTTClient() mqtt.Client {
-	opts := mqtt.NewClientOptions()
-	opts.AddBroker(mqttBrokerAddr)
-	opts.SetClientID(mqttClientID)
-
-	tlsConfig, err := tlsutil.BuildTLSConfig()
-	if err != nil {
-		log.Fatalf("Failed to build TLS config: %s", err)
-	}
-	opts.SetTLSConfig(tlsConfig)
-
-	opts.SetAutoReconnect(true)
-	opts.SetMaxReconnectInterval(5 * time.Second)
-
-	opts.SetCleanSession(true)
-	opts.SetKeepAlive(2 * time.Second)
-
-	opts.SetConnectRetry(true)
-	opts.SetConnectRetryInterval(5 * time.Second)
-
-	opts.SetConnectionAttemptHandler(func(broker *url.URL, tlsCfg *tls.Config) *tls.Config {
-		log.Printf("%s attempting to connect to MQTT broker: %s\n", mqttClientID, broker)
-		return tlsCfg
-	})
-	opts.SetOnConnectHandler(func(client mqtt.Client) {
-		log.Printf("%s connected to MQTT broker: %s\n", mqttClientID, mqttBrokerAddr)
-	})
-	opts.SetConnectionLostHandler(func(client mqtt.Client, err error) {
-		log.Printf("%s lost connection to MQTT broker: %s - %s\n", mqttClientID, mqttBrokerAddr, err)
-	})
-
-	return mqtt.NewClient(opts)
 }
